3eae79c091aa728b6dee18e2281de625c9116107
rajavel771980 <rajavel771980@gmail.com>
2024-08-20 15:29:53 +0530

    form validations

--- frontend/src/views/Dashboard/ActionsCard.vue
+++ frontend/src/views/Dashboard/ActionsCard.vue
@@ -2,10 +2,12 @@
 import { api } from '@/api';
 import { useModal } from '@/composables/useModal';
 import { useToast } from '@/composables/useToast';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 const modal = useModal<boolean>();
 const toast = useToast();
 const applicationFormRef = ref();
+const startValidation = ref(false);
+
 let errors = []
 
 const formData = ref({
@@ -27,90 +29,133 @@ const formData = ref({
   savingsContribution: 0,
 });
 
-const validateFormData = () => {
-  console.log('validate formData', formData.value);
-  this.errors = [];
 
-  if (!formData.value.applicantName) {
-    this.errors.push('Name is required');
+const isValidApplicantName = computed(() => {  
+  if (startValidation.value) {
+    return /^[A-Za-z\s]{3,20}$/.test(formData.value.applicantName);;  
   }
+  return true;
+})
 
-  if (!formData.value.applicantEmail) {
-    this.errors.push('Email is required');
+const isValidEmail = computed(() => {
+  if (startValidation.value) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.value.applicantEmail);
   }
+  return true;
+});
 
-  if (!formData.value.applicantMobilePhoneNumber) {
-    this.errors.push('Mobile Phone Number is required');
+const isValidPhoneNumber = computed(() => {
+  if (startValidation.value) {    
+    return /^\(\+\d{1,4}\)\s\d{10}$/.test(formData.value.applicantMobilePhoneNumber);
   }
+  return true;
+});
 
-  if (!formData.value.applicantAddress) {
-    this.errors.push('Address is required');
+const isValidAddress = computed(() => {
+  if (startValidation.value) {
+    return /^[A-Za-z0-9\s\.\-,]+$/.test(formData.value.applicantAddress);
   }
+  return true;
+});
 
-  if (!formData.value.annualIncomeBeforeTax) {
-    this.errors.push('Annual Income Before Tax is required');
+const isValidIncome = computed(() => {
+  if (startValidation.value) {
+    return formData.value.annualIncomeBeforeTax > 0;
+  } 
+  return true; 
+});
+
+const isValidIncomingAddress = computed(() => {
+  if (startValidation.value) {
+    return /^[A-Za-z0-9\s\.\-,]+$/.test(formData.value.incomingAddress); 
+  }  
+  return true;
+})
+
+const isValidIncomingDeposit = computed(() => {
+  if (startValidation.value) {
+    return formData.value.incomingDeposit > 0;
+  }  
+  return true;
+});
+
+const isValidIncomingPrice = computed(() => {
+  if (startValidation.value) {
+    return formData.value.incomingPrice > 0;  
   }
+  return true;
+});
 
-  if (!formData.value.incomingAddress) {
-    this.errors.push('Incoming Address is required');
+const isValidIncomingStampDuty = computed(() => {
+  if (startValidation.value) {
+    return formData.value.incomingStampDuty > 0;  
   }
+  return true;
+});
 
-  if (!formData.value.incomingDeposit) {
-    this.errors.push('Incoming Deposit is required');
+const isValidLoanAmount = computed(() => {
+  if (startValidation.value) {
+    return formData.value.loanAmount > 0;
   }
+  return true;
+});
 
-  if (!formData.value.incomingPrice) {
-    this.errors.push('Incoming Price is required');
+const isValidLoanDuration = computed(() => {
+  if (startValidation.value) {
+    return formData.value.loanDuration > 0;
   }
+  return true;
+});
 
-  if (!formData.value.incomingStampDuty) {
-    this.errors.push('Incoming Stamp Duty is required');
+const isValidMonthlyExpenses = computed(() => {
+  if (startValidation.value) {
+    return formData.value.monthlyExpenses > 0;
   }
+  return true;
+}); 
 
-  if (!formData.value.loanAmount) {
-    this.errors.push('Loan Amount is required');
+const isValidOutgoingAddress = computed(() => {
+  if (startValidation.value) {
+    return /^[A-Za-z0-9\s\.\-,]+$/.test(formData.value.outgoingAddress);
   }
+  return true;
+});
 
-  if (!formData.value.loanDuration) {
-    this.errors.push('Loan Duration is required');
+const isValidOutgoingMortgage = computed(() => {
+  if (startValidation.value) {
+    return formData.value.outgoingMortgage > 0;
   }
+  return true;
+}); 
 
-  if (!formData.value.monthlyExpenses) {
-    this.errors.push('Monthly Expenses is required');
+const isValidOutgoingValuation = computed(() => {
+  if (startValidation.value) {
+    return formData.value.outgoingValuation > 0;
   }
+  return true;
+});
 
-  if (!formData.value.outgoingAddress) {
-    this.errors.push('Outgoing Address is required');  
+const isValidSavingsContribution = computed(() => {
+  if (startValidation.value) {
+    return formData.value.savingsContribution > 0;
   }
+  return true;
+});
 
-  if (!formData.value.outgoingMortgage) {
-    this.errors.push('Outgoing Mortgage is required');
+const isValidForm = computed(() => {
+  return isValidApplicantName.value && isValidEmail.value && isValidPhoneNumber.value && isValidAddress.value &&
+    isValidIncome.value && isValidIncomingAddress.value && isValidIncomingDeposit.value && isValidIncomingPrice.value && isValidIncomingStampDuty.value && isValidLoanAmount.value && isValidLoanDuration.value && isValidMonthlyExpenses.value && isValidOutgoingAddress.value && isValidOutgoingMortgage.value && isValidOutgoingValuation.value && isValidSavingsContribution.value;
+});
+
+const submitApplication = async () => {
+  startValidation.value = true;
+  if (!isValidForm.value) {
+    return;
   }
-
-  if (!formData.value.outgoingValuation) {
-    this.errors.push('Outgoing Valuation is required');
-  }
-
-  if (!formData.value.savingsContribution) {
-    this.errors.push('Savings Contribution is required');
-  }
-
-  console.log('errors', this.errors);
-
-  return errors.length === 0
-}
-
-const submitApplication = async (event) => {
-  event.preventDefault();
-
   try {
-    const valid = validateFormData();
 
-    if (!valid) return;
-
-    console.log('formData', formData.value);
     const response = await api.applications.post(formData.value);
-    if (response.success) toast.success('Application Saved Successfully.');
+    if (response.success) toast.success(`Application Saved Successfully.<br> Returned Loan Amount : $${response.loanAmount}`);
     else {
       toast.error('Error occurred while saving application');
       formData.value.applicantName = '';
@@ -132,12 +177,12 @@ const submitApplication = async (event) => {
     }
     modal.confirm(true)
 
-    // Send data to server
-  } catch (err) {
-    console.log(err.errors);
-    toast.error('Error occurred while saving application');
+  } catch (error) {
+    toast.error('An error occurred');
   }
-};
+}
+
+
 </script>
 
 <template>
@@ -168,9 +213,11 @@ const submitApplication = async (event) => {
         <!-- Need to change with v-for after change state with object -->
         <label for="applicant_name">Name</label>
         <BTextInput v-model="formData.applicantName" id="applicant_name" type="text" required />
+        <div class="error">{{ isValidApplicantName? '' : 'Please enter a valid name' }}</div>
 
         <label for="applicant_email">Email</label>
         <BTextInput v-model="formData.applicantEmail" id="applicant_email" type="email" required />
+        <div class="error">{{ isValidEmail? '' : 'Please enter a valid email address' }}</div>
 
         <label for="applicant_mobile_phone_number">Mobile Phone Number</label>
         <BTextInput
@@ -179,34 +226,59 @@ const submitApplication = async (event) => {
           type="tel"
           required
         />
+        <div class="error">{{ isValidPhoneNumber? '' : 'Please enter a valid phone number' }}</div>
 
         <label for="applicant_address">Applicant Address</label>
         <BTextInput v-model="formData.applicantAddress" id="applicant_address" required />
+        <div class="error">{{ isValidAddress? '' : 'Please enter a valid address' }}</div>
+
         <label for="annual_income_before_tax">Annual Income Before Tax</label>
         <BNumberInput v-model="formData.annualIncomeBeforeTax" id="annual_income_before_tax" required />
+        <div class="error">{{ isValidIncome? '' : 'Please enter a valid income' }}</div>
+
         <label for="incoming_address">Incoming Address</label>
         <BTextInput v-model="formData.incomingAddress" id="incoming_address" required />
+        <div class="error">{{ isValidIncomingAddress? '' : 'Please enter a valid address' }}</div>
+
         <label for="incoming_deposit">Incoming deposit</label>
         <BNumberInput v-model="formData.incomingDeposit" id="incoming_deposit" required />
+        <div class="error">{{ isValidIncomingDeposit? '' : 'Please enter a valid deposit' }}</div>
 
         <label for="incoming_price">Incoming Price</label>
         <BNumberInput v-model="formData.incomingPrice" id="incoming_price" required />
+        <div class="error">{{ isValidIncomingPrice? '' : 'Please enter a valid price' }}</div>
+
         <label for="incoming_stamp_duty">Incoming Stamp Duty</label>
         <BNumberInput v-model="formData.incomingStampDuty" id="incoming_stamp_duty" required />
+        <div class="error">{{ isValidIncomingStampDuty? '' : 'Please enter a valid stamp duty' }}</div>
+
         <label for="loan_amount">Loan Amount</label>
         <BNumberInput v-model="formData.loanAmount" id="loan_amount" required />
+        <div class="error">{{ isValidLoanAmount? '' : 'Please enter a valid loan amount' }}</div>
+
         <label for="loan_duration">Loan Duration</label>
         <BNumberInput v-model="formData.loanDuration" id="loan_duration" required />
+        <div class="error">{{ isValidLoanDuration? '' : 'Please enter a valid loan duration' }}</div>
+
         <label for="monthly_expenses">Monthly Expenses</label>
         <BNumberInput v-model="formData.monthlyExpenses" id="monthly_expenses" required />
+        <div class="error">{{ isValidMonthlyExpenses? '' : 'Please enter a valid monthly expenses' }}</div>
+
         <label for="outgoing_address">Outgoing Address</label>
         <BTextInput v-model="formData.outgoingAddress" id="outgoing_address" required />
+        <div class="error">{{ isValidOutgoingAddress? '' : 'Please enter a valid address' }}</div>
+
         <label for="outgoing_mortgage">Outgoing Mortgage</label>
         <BNumberInput v-model="formData.outgoingMortgage" id="outgoing_mortgage" required />
+        <div class="error">{{ isValidOutgoingMortgage? '' : 'Please enter a valid mortgage' }}</div>
+
         <label for="outgoing_valuation">Outgoing Valuation</label>
         <BNumberInput v-model="formData.outgoingValuation" id="outgoing_valuation" required />
+        <div class="error">{{ isValidOutgoingValuation? '' : 'Please enter a valid valuation' }}</div>
+
         <label for="savings_contribution">Savings Contribution</label>
         <BNumberInput v-model="formData.savingsContribution" id="savings_contribution" required />
+        <div class="error">{{ isValidSavingsContribution? '' : 'Please enter a valid contribution' }}</div>
       </form>
 
       <template #footer>
@@ -243,5 +315,10 @@ const submitApplication = async (event) => {
 .b-icon {
   width: 5rem;
   height: 5rem;
+}
+.error {
+  color: red;
+  font-size: 0.8rem;
+  margin-bottom: 0.5rem;
 }
 </style>
